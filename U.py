625b120499aa6d3f7323497a7635472f7d9ad24a
QGB <qgbcs1@gmail.com>
2015-11-09 02:52:12 +0800

    single

--- U.py
+++ U.py
@@ -1,9 +1,56 @@
+# coding=utf-8
 from ctypes import windll#.user32.MessageBoxA as m
 u=windll.user32
 msgbox=windll.user32.MessageBoxA
-import  os,sys
+import  os,sys,socket
+
+from threading import *
 #m(0, 'rtegwf', 'hi', 0)
 #print 
+'''
+from qgb import U,T
+'''
+
+SG_EXIT='exit'
+SG_ASK='ask'
+
+def __single(port,callback,reply):
+	sock=None
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+	sock.bind(('0.0.0.0', port))
+	sock.listen(5) 
+	while True:    
+		connection,address = sock.accept()    
+		try:    
+			connection.settimeout(50)
+			buf=''
+			while buf!=None:  
+				buf = connection.recv(4096) 
+				if(reply!=None):connection.send(reply+buf)
+				callback(buf)
+		except Exception as e:  #如果建立连接后，该连接在设定的时间内无数据发来，则time out  
+			 print e  
+			 continue
+	connection.close()   
+
+def isingle(port):
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+		sock.connect(('localhost', port))
+		sock.send(SG_ASK)    
+		sock.close()
+		return False
+	except Exception:
+		return True
+def single(port,callback,reply='\naccepted:'):
+	Thread(target=__single,args=[port,callback,reply]).start()
+
+import inspect
+def fields(obj):
+	return inspect.getmembers(obj)
+
+def methods(obj):
+	return dir(obj)
 
 def msgbox(s='',st='title',*a):
 	if(a!=()):s=str(s)+ ','+str(a)[1:-2]
@@ -28,35 +75,3 @@ def exit(i=2357):
 	os._exit(i)
 
 
-def test(a='mmmmmmmmm'):
-	i=0
-	s='''
-print 1122
-print dir()
-print a
-print s
-i+=1
-print '-'*6*5+str(i)
-if(i<4):
-	exec(s) 
-	'''
-	exec(s)
-	exit()
-	
-#test()
-
-#msgbox('1111')
-'''
-test('76543')
-
-
-#
-t=test()x
-
-print id(test),id(t)
-os._exit(3)
-print type(t),type(test)
-try:t()
-except Exception as e:print str(e)
-'''
-#print type(t)
