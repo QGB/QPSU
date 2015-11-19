3a8916900de4847810687db58e2de8a38e533ee2
QGB <qgbcs1@gmail.com>
2015-11-19 21:37:09 +0800

    setout

--- U.py
+++ U.py
@@ -5,22 +5,55 @@ msgbox=windll.user32.MessageBoxA
 import  os,sys,socket
 
 from threading import *
+
 #m(0, 'rtegwf', 'hi', 0)
 #print 
 '''
 from qgb import U,T
-'''
+# '''
+# if __calltimes=0
 BDEBUG=True
 __stdout=None
+
+
+
 def setOut(afileName):
 	__stdout,sys.stdout=sys.stdout,open(afileName,'w+')
 
+
 def resetOut():
+	# msgbox(__stdout)
 	if(__stdout != None and __stdout != sys.stdout):
 		sys.stdout.close()
-		sys.stdout=__stdout
+		# sys.stdout=__stdout
+txthtml=('<textarea style="width:100%; height:100%;">','</textarea>')	
+
+def browser(url):
+	os.system('''start "" '''+str(url))
+def shtml(file,txt,browser=True):
+	f=open(file,'w+')
+	txt=txt.replace(txthtml[1],txthtml[1][:1]+'!!!qgb-padding!!!'+txthtml[1][1:])	
+	f.write(txthtml[0]+txt+txthtml[1])
+	f.close()
+	globals()['browser'](f.name)
+def dicthtml(file,dict,aikeylength=10,browser=True):
+	for i in dict.keys():
+		if(len(i)>aikeylength):aikeylength=len(i)+1
+	sformat='%-'+str(aikeylength)+'s:'
+	txt=''
+	for i in dict.keys():
+		txt+= (sformat%i)+str(dict[i])+'\n'
+	txt=txt.replace(txthtml[1],txthtml[1][:1]+'!!!qgb-padding!!!'+txthtml[1][1:])	
+	f=open(file,'w+')
+	f.write(txthtml[0]+txt+txthtml[1])
+	f.close()
+	globals()['browser'](f.name)
+	# print vars()
+	# vars()['browser'](f.name)
+
+# dicthtml('uvars.html',vars())
 	
-	
+
 	
 def getThreads():
 	r=()
