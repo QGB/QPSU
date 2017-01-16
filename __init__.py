d7df999be028a9c548751f44cfde44944482adc9
QGB <qgbcs1@gmail.com>
2017-01-16 14:21:05 +0800

    ipy_autocall

--- __init__.py
+++ __init__.py
@@ -17,3 +17,11 @@ for i in __all__:
 	except:__all__.remove(i)
 	
 # print __all__
+try:
+	import sys
+	f=sys._getframe()
+	while f and f.f_globals and 'get_ipython' not in f.f_globals.keys():
+		f=f.f_back
+	ipy=f.f_globals['get_ipython']()
+	ipy.autocall=2
+except Exception as e:print e
