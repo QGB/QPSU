d2a7a00ff993ccb5303a45b7d3df3fc113554a89
QGB <qgbcs1@gmail.com>
2019-04-10 12:47:10 +0800

    useragent

--- N/HTTP.py
+++ N/HTTP.py
@@ -48,7 +48,7 @@ def getBytes(url):
 		return py.No(e)
 	
 def get(url,file='',
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.2171.95 Safari/537.36'},
+		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'},
 	):
 	# return method(url,'get')#<http.client.HTTPResponse at 0x203a16a74a8>
 	url=autoUrl(url)
