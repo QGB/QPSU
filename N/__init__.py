7f2b057c8b258e0537e92a46cb44d483aeb80c19
QGB <qgbcs1@gmail.com>
2019-09-09 09:19:51 +0800

    N.whois

--- N/__init__.py
+++ N/__init__.py
@@ -296,6 +296,33 @@ pip install qqwry  # Not have cz88update
 	return ip_location_qqwry.q.lookup(ip)  #('北京市', '联通')
 
 ###################  qqwry end ###########################
+def whois(domain,raw_response=False):
+	T=py.importT()
+	import requests
+	cookies = {	'st': '95f5609d7aaadb13806df43e2ca1961c',	}
+	headers = {
+		'Pragma': 'no-cache',
+		'Origin': 'http://whois.4.cn',
+		'Accept-Encoding': 'gzip, deflate',
+		'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7',
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
+	'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 YaBrowser/19.3.1.779 Yowser/2.5 Safari/537.36',
+
+		'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+		'Accept': 'application/json, text/javascript, */*; q=0.01',
+		'Cache-Control': 'no-cache',
+		'X-Requested-With': 'XMLHttpRequest',
+		'Connection': 'keep-alive',
+		'Referer': 'http://whois.4.cn/batch',
+	}
+
+	data = {'domain': domain,  'isRefresh': '1'}
+	resp=requests.post('http://whois.4.cn/api/main',headers=headers,cookies=cookies,data=data)
+	json=T.json_loads(resp.content.decode('utf-8'))
+	if raw_response:
+		return resp
+	return json
+	
 	
 def bulk_whois(domain_list,args):
 	import requests,json,logging
