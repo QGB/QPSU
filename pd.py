1c53980f30e537f4c1004a1539720b84524e8af4
qgb <qgbcs1@gmail.com>
2021-11-15 12:15:52 +0800

    pandas pd.py

--- pd.py
+++ pd.py
@@ -2,8 +2,8 @@ from pandas import *
 
 def df_to_namedtuples(df):
 	from collections import namedtuple
-    Row = namedtuple('Row', df.columns)
-    for row in df.itertuples():
-        yield Row(*row[1:])
+	Row = namedtuple('Row', df.columns)
+	for row in df.itertuples():
+		yield Row(*row[1:])
 yield_namedtuple=df2namedtuples=df_to_namedtuples		
 
