c07e04c0209890c33de9218b0501900882532647
qgb <qgbcs1@gmail.com>
2021-11-15 12:14:28 +0800

    float special vlaues

--- pd.py
+++ pd.py
@@ -1,8 +1,9 @@
-from collections import namedtuple
+from pandas import *
 
 def df_to_namedtuples(df):
+	from collections import namedtuple
     Row = namedtuple('Row', df.columns)
     for row in df.itertuples():
         yield Row(*row[1:])
-df2namedtuples=df_to_namedtuples		
+yield_namedtuple=df2namedtuples=df_to_namedtuples		
 
