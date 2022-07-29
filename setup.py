1056be5067336d604edfd874e742c56a138fc510
qgb <qgbcs1@gmail.com>
2022-07-30 02:07:26 +0800

    package_dir

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ print('#'*99,find_packages())
 
 setup(
     name="qgb",
-    version="0.01",
+    version="0.1",
     packages=['qgb'],
     package_dir={
         'qgb': '.',
+        'qgb.N': './N',
     },
 )
 
